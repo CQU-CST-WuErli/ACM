a18a559a59c4d44be67e22130734605f73150461
CQU_CST_WuErli <854175782@qq.com>
2016-07-15 18:17:22 +0800

    CF

---
+++ CodeForces/#362_div2/E.cpp
@@ -0,0 +1,89 @@
+//
+//  Created by  CQU_CST_WuErli
+//  Copyright (c) 2016 CQU_CST_WuErli. All rights reserved.
+//
+//#pragma comment(linker, "/STACK:102400000,102400000")
+#include <iostream>
+#include <cstring>
+#include <cstdio>
+#include <cstdlib>
+#include <cctype>
+#include <cmath>
+#include <string>
+#include <vector>
+#include <map>
+#include <queue>
+#include <stack>
+#include <set>
+#include <algorithm>
+#include <sstream>
+#define CLR(x) memset(x,0,sizeof(x))
+#define OFF(x) memset(x,-1,sizeof(x))
+#define MEM(x,a) memset((x),(a),sizeof(x))
+#define BUG cout << "I am here" << endl
+#define lookln(x) cout << #x << "=" << x << endl
+#define SI(a) scanf("%d", &a)
+#define SII(a,b) scanf("%d%d", &a, &b)
+#define SIII(a,b,c) scanf("%d%d%d", &a, &b, &c)
+const int INF_INT=0x3f3f3f3f;
+const long long INF_LL=0x7f7f7f7f;
+const int MOD=1e9+7;
+const double eps=1e-10;
+const double pi=acos(-1);
+typedef long long  ll;
+using namespace std;
+
+int n;
+ll a[100010];
+
+ll ksm(ll a, ll x) {
+	if (x == 0LL) return 1LL % MOD;
+	ll ret = ksm(a, x / 2);
+	ret = ret * ret % MOD;
+	if (x & 1) ret = ret * a % MOD;
+	return ret;
+}
+
+int main(int argc, char const *argv[]) {
+#ifdef LOCAL
+    freopen("C:\\Users\\john\\Desktop\\in.txt","r",stdin);
+    // freopen("C:\\Users\\john\\Desktop\\out.txt","w",stdout);
+#endif
+    while (SI(n) == 1) {
+    	for (int i = 1; i <= n; i++)
+    		scanf("%I64d", a + i);
+    	ll p, q = 2LL;
+    	int flag = 0;
+    	for (int i = 1; i <= n; i++) {
+    		q = ksm(q, a[i]);
+    		if (!(a[i] & 1)) flag = 1;
+    	}
+    	q = q * ksm(2, MOD - 2) % MOD;
+    	p = (q + MOD + (flag ? 1 : -1)) % MOD;
+    	p = p * ksm(3, MOD - 2) % MOD;
+    	cout << p << '/' << q << endl;
+    }
+	return 0;
+}
+/*
+                   _ooOoo_
+                  o8888888o
+                  88" . "88
+                  (| -_- |)
+                  O\  =  /O
+               ____/`---'\____
+             .'  \|     |//  `.
+            /  \|||  :  |||//  \
+           /  _||||| -:- |||||-  \
+           |   | \\  -  /// |   |
+           | \_|  ''\---/''  |   |
+           \  .-\__  `-`  ___/-. /
+         ___`. .'  /--.--\  `. . __
+      ."" '<  `.___\_<|>_/___.'  >'"".
+     | | :  `- \`.;`\ _ /`;.`/ - ` : | |
+     \  \ `-.   \_ __\ /__ _/   .-` /  /
+======`-.____`-.___\_____/___.-`____.-'======
+                   `=---='
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+			佛祖保佑		永无BUG
+*/
